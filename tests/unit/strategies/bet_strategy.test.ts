35f36059355955c14c7b50466a3edb5d141932d1
andycave <andy.cave@nearform.com>
2026-08-19 11:46:32 +0100

    feat: add probability

--- tests/unit/strategies/bet_strategy.test.ts
+++ tests/unit/strategies/bet_strategy.test.ts
@@ -1,8 +1,9 @@
 import type { Strategy } from '../../../src/strategies/types';
 import type { SourceMatch } from '../../../src/sources/types';
 import { StubProbability } from '../utils';
-import type { Prediction, Probability } from '../../../src/strategies/probabilities/types';
+import type { Probability } from '../../../src/strategies/probabilities/types';
 import { BetStrategy } from '../../../src/strategies/strategies';
+import type { MatchBetType } from '../../../src/bets/types';
 
 describe('Given a strategy with a probability calculation', () => {
   let strategy: Strategy;
@@ -14,12 +15,15 @@ describe('Given a strategy with a probability calculation', () => {
     match = { home: 'Arsenal', away: 'Chelsea' };
   });
   describe('when a bet is created for a match', () => {
-    let prediction: Prediction;
+    let bet: MatchBetType;
     beforeEach(() => {
-      prediction = strategy.bet(match).probability;
+      bet = strategy.bet(match);
     });
     it('returns the calculated probability in the bet', () => {
-      expect(prediction).toStrictEqual({ home: 0.3, away: 0.3, draw: 0.4 });
+      expect(bet.probability).toStrictEqual({ home: 0.3, away: 0.3, draw: 0.4 });
+    });
+    it('assigns the match teams to the bet', () => {
+      expect(bet.teams).toStrictEqual(match);
     });
   });
 });
