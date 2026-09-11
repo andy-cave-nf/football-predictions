88f2f47f3d97c146c401de20149561c15b720d17
andycave <andy.cave@nearform.com>
2026-09-11 15:47:36 +0100

    feat: add fixed ratings

--- src/strategies/probabilities/calculations.ts
+++ src/strategies/probabilities/calculations.ts
@@ -1,3 +1,22 @@
 import type { OutcomeDistribution } from '../../shared';
 
 export type ProbabilityCalculation<T> = (home: T, away: T) => OutcomeDistribution;
+
+export const EloCalculation: ProbabilityCalculation<number> = (
+  home: number,
+  away: number
+): OutcomeDistribution => {
+  const kappa = 1.5;
+  const s = 400;
+  const d = home - away;
+
+  const homeTerm = Math.pow(10, d / s);
+  const awayTerm = Math.pow(10, -d / s);
+  const denom = homeTerm + awayTerm + kappa;
+
+  return {
+    home: homeTerm / denom,
+    draw: kappa / denom,
+    away: awayTerm / denom,
+  };
+};
--- src/strategies/probabilities/ratings.ts
+++ src/strategies/probabilities/ratings.ts
@@ -1,5 +1,6 @@
 import type { MatchSource } from '../../shared';
 import type { Ratings, RatingsSource, TeamMapping } from './types';
+import { readFile } from 'node:fs/promises';
 
 export class MappedRatings<T> implements Ratings<T> {
   constructor(
@@ -8,5 +9,35 @@ export class MappedRatings<T> implements Ratings<T> {
   ) {}
   async ratingFor(id: string, source: MatchSource): Promise<T> {
     return await this.source.ratingFor(await this.mapping.mappedId(id, source));
+  }
+}
+
+export class FixedEloRatings implements RatingsSource<number> {
+  constructor(private filepath: string) {}
+  async ratingFor(id: string): Promise<number> {
+    const ratings: Record<string, number> = JSON.parse(await readFile(this.filepath, 'utf8'));
+    const rating = ratings[id];
+    if (rating === undefined) {
+      throw new Error('Missed Rating!');
+    }
+    return rating;
+  }
+}
+
+export class FixedEloMappedRatings implements TeamMapping {
+  constructor(private filepath: string) {}
+  async mappedId(id: string, source: MatchSource): Promise<string> {
+    const mappings: Record<string, Record<string, string>> = JSON.parse(
+      await readFile(this.filepath, 'utf8')
+    );
+    const map = mappings[source];
+    if (map === undefined) {
+      throw new Error('Missed Rating!');
+    }
+    const name = map[id];
+    if (name === undefined) {
+      throw new Error('Missed Rating!');
+    }
+    return name;
   }
 }
